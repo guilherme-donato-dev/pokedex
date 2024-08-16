385b5455b5f744ee227ed1c434a1bfb907057648
guilherme-donato-dev <donatocruzg@gmail.com>
2024-08-16 17:40:59 -0300

    adicionado o squirtle

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from pokedex.models import Pokemon
 # Register your models here.
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display =  ('name', 'type')
+    #usado para listar os campos que vão aparecer no registro do carro
+
+admin.site.register(Pokemon, PokemonAdmin)
--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='pokedex/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
